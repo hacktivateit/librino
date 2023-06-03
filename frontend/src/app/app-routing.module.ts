e5d6fc8329be82702c5d576ff1d60f953654d26e
hacktivateit <matomoto32@gmail.com>
2023-06-03 12:47:48 +0200

    fixed imports

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { BookComponent } from './components/book/book.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'register/', component: RegisterComponent },
-  { path: 'login/', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'booklist/:id', component: BooklistComponent },
   { path: 'book/:id', component: BookComponent },
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,22 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { RegisterComponent } from './components/register/register.component';
-import { AppComponent } from './app.component';
-import { AddBookComponent } from './components/add-book/add-book.component';
-import { TopBarComponent } from './components/top-bar/top-bar.component';
-
 //Material
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { AppRoutingModule } from './app-routing.module';
+import { RegisterComponent } from './components/register/register.component';
+import { AppComponent } from './app.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { BooklistComponent } from './components/booklist/booklist.component';
 import { BookComponent } from './components/book/book.component';
 import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
