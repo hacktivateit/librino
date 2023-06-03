91eb3fc2d85f4537585372d1e4ec128e9f64c301
hacktivateit <matomoto32@gmail.com>
2023-06-03 20:11:27 +0200

    Auth services

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:4242/api/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  //Data user instead of any?
+  signup(data: any): Observable<any> {
+    return this.http.post(`${baseUrl}/signup`,data)
+  }
+
+  signin(data: any): Observable<any>{
+    return this.http.post(`${baseUrl}/signin`,data)
+  }
+
+  signout(id: Number): Observable<any> {
+    return this.http.post(`${baseUrl}/signout`,id)
+  }
+}
+
