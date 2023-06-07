b01f2c70b008a4f1f45c5227827b5f837b7fd000
hacktivateit <matomoto32@gmail.com>
2023-06-07 18:04:05 +0200

    Small adjustment but CORS still not working

--- backend/index.ts
+++ backend/index.ts
@@ -3,19 +3,25 @@ import dotenv from 'dotenv';
 import bookRouter from './routes/book';
 import authRouter from './routes/auth';
 import checkAuth from './middlewares/checkAuth';
+import cors from 'cors';
 
 // Read config from .env
 dotenv.config()
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// This SHOULD enable cors for every domain
+// https://expressjs.com/en/resources/middleware/cors.html#simple-usage-enable-all-cors-requests
+app.use(cors());
+
 // Use express json middleware
 app.use(express.json());
 
 app.use('/api/auth/',authRouter);
 app.use('/api/book/',checkAuth,bookRouter);
 //Used for debug
-// app.use('/api/user/',checkAuth,userRouter);
+//app.use('/api/user/',checkAuth,userRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, this is librino api backend, use the endpoint /api to talk with me');
